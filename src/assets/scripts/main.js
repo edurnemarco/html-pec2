e15270395e44dfcb2d0047c56cb549c6a7c12b99
Edurne Marco García <emarco35@uoc.edu>
2025-04-29 20:00:26 +0200

    pages article and colabora

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,7 +11,35 @@ import * as bootstrap from 'bootstrap';
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+// Modal for the colabora form:
+
+const form = document.querySelector('form');
+if (form) {
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const modal = new bootstrap.Modal(document.getElementById('thankYouModal'));
+    modal.show();
+  });
+}
+
+// Carousel gallery
+
+import '@splidejs/splide/dist/css/splide.min.css';
+
+import Splide from '@splidejs/splide';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const el = document.querySelector('.splide');
+  if (el) {
+    new Splide(el, {
+      type   : 'loop',
+      perPage: 3,
+      gap    : '1rem',
+      breakpoints: {
+        768: {
+          perPage: 1,
+        },
+      },
+    }).mount();
+  }
+});
