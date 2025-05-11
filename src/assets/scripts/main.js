4ab9ae71f37ff117f11730d48b67b728495b7d3e
Edurne Marco García <emarco35@uoc.edu>
2025-05-11 18:11:25 +0200

    last changes after validation

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,15 +10,25 @@ import * as bootstrap from 'bootstrap';
 /**
  * Write any other JavaScript below
  */
-
 // Modal for the colabora form:
 
 const form = document.querySelector('form');
 if (form) {
   form.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    const modal = new bootstrap.Modal(document.getElementById('thankYouModal'));
-    modal.show();
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+    .then(() => {
+      const modal = new bootstrap.Modal(document.getElementById('thankYouModal'));
+      modal.show();
+      form.reset();
+    })
+    .catch((error) => alert("Error al enviar el formulario"));
   });
 }
 
